d7c27fc69fb8c7c2d554e52f806be0e1ca2e39f8
Ayşe Ulaşlı <ayseulasli2727@hotmail.com>
2026-03-31 21:59:49 +0300

    GYGY 5.0 macerası başlıyor

---
+++ intro/src/main/java/com/turkcell/Main.java
@@ -0,0 +1,8 @@
+package com.turkcell;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Merheba Turkcell!");
+        System.out.println("GYGY 5.0 da görüşmek üzere!");
+    }
+}
