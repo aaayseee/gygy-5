1bfdac2647ce9e4a3284ed057ba13fecd1e9909b
Ayşe Ulaşlı <ayseulasli2727@hotmail.com>
2026-04-06 15:06:53 +0300

    Methodlar ve OOP'ye Giriş

---
+++ intro/src/main/java/com/turkcell/Car.java
@@ -0,0 +1,28 @@
+package com.turkcell;
+
+public class Car {
+    public int year;
+    public String model;
+    protected String brand;
+    // erişim belirteci: o alana kimlerin erişebileceğini belirleyen sistem
+    private double price; 
+    // public => her yerden erişilebilir
+    // private => sadece tanımlanan sınıf içerisinden erişilebilir.
+    // protected => sadece tanımlanan sınıf ve o sınıftan (türetilen) sınıflardan erişilebilir.
+    // protected => aynı paketteki sınıflardan erişilebilir.
+
+    // Encapsulation
+    //
+    public double getPrice(){
+        // get işlemlerini kontrol eden mekanizma
+        return price;
+    }
+    public void setPrice(double price) {
+        // classın kendisi = price
+        if(price < 0){
+            System.out.println("Fiyat negatif olamaz!");
+            return; // metodu sonlandırmak için kullanılır, geri kalan kodların çalışmasını engeller
+        }
+        this.price = price;
+    }
+}
--- intro/src/main/java/com/turkcell/Main.java
+++ intro/src/main/java/com/turkcell/Main.java
@@ -138,5 +138,107 @@ public class Main {
             whileDongusu++;
         }
 
+
+
+        // Karar Blokları
+
+        // Belirli 1+ kapsamdaki kod bloklarını belirli koşullara göre ateşlemek.
+        // Karar bloğu = minimum 1 maksimum n adet karara göre farklı kodlar çalıştırabilir.
+        // Koşul: true-false
+
+        // Her koşul bloğu yalnızca maksimum 1 scope çalıştırır.
+        // Kodlar yukarıdan aşağıya çalıştırılır.
+        int age2 = 18;
+
+        if (age2 >= 18) {
+            System.out.println("Yetki verildi");
+        }
+        else if (age2 == 18) {
+            System.out.println("Yaşınız tam 18, ay kontrolü yapılıyor.");
+        }
+        else {
+            System.out.println("Yetki verilmedi");
+        }
+
+        String username = "halit";
+        if(username.equals("tamer"))
+        {
+            System.out.println("Tamer hoş geldin..");
+        } // Karar blokları illaki bir scope çalıştırma zorunluluğu barındırmaz.
+
+
+         /// ....... öğrenci notu hesaplama
+        String result1 = calculateGrade(85); // konsola yaz
+        String result2 = calculateGrade(70, "Ayşe"); // db'e yaz gibi farklı işlemler yapılabilir.
+        String result3 = calculateGrade(60); // email at..
+        String result4 = calculateGrade(50, "Nurgül");
+        String result5 = calculateGrade(30, "Tamer");
+
+        System.out.println(result1); // result1 konsola yazar ve diğer resultlar farklı işlemler yapabilir.
+
+        // result2 db'e yaz
+        // result3 mail at..
+
+
+
+        double toplam1 = sum(10.5, 20.3);
+        System.out.println(toplam1);
+
+        double toplam2 = sum(1,2,3,4,5,6,7);
+        System.out.println(toplam2);
+
     }
+
+
+    // Methodlar => belirli bir işi yapan kod bloklarıdır. Tekrar tekrar kullanılabilirler.
+    // erişim-belirteci - static veya boş - dönüş tipi (void => boş) - method ismi - (parametreler) - {}
+    // bir parametre tanımlıysa, null bile olsa göndermek zorundasın.
+    public static String calculateGrade(int grade, String name) // required parameter
+    {
+        if(grade >= 85)
+        {
+            String result = name + " Notunuz: A";
+            return result; //dönüş tipi String olduğu için, String bir değer döndürmelidir bu yüzden return kullanılır.
+            //System.out.println(name + " Notunuz: A"); // return kullanıldığında, methodun geri kalan kodları çalışmaz.
+        }
+        else if(grade >= 70)
+        {
+            return name + " Notunuz: B";
+        }
+        else if(grade >= 50)
+        {
+            return name + " Notunuz: C";
+        }
+        else
+        {
+            return name + " Notunuz: F";
+        }
+    }
+
+    // Name gönderilmezse, "Öğrenci" olarak varsayılan değer alsın.
+    // Method Overloading => Aynı isimde, farklı parametre sayısına sahip methodlar oluşturma.
+    public static String calculateGrade(int grade)
+    {
+        return calculateGrade(grade, "Öğrenci");
+    }
+
+    //Sayıları toplayan sum methodu
+     public static double sum(double a, double b){ //Burda sadece iki sayı toplayabiliriz
+        return a + b;
+    }
+
+    public static double sum(double... numbers) { // varargs sayesinde istediğimiz kadar sayı toplayabiliriz, numbers bir dizi olarak çalışır.
+        double total = 0;
+        for(double num: numbers) {
+            total += num;
+        }
+        return total;
+    }
+
 }// Main classının kapsama alanı (sınır)
+
+
+// CTRL + SHIFT + P
+// User Settings
+// Compact
+// Explorer: Compact Folders kapat Bu sayede klasörler tek tek görünecektir.
