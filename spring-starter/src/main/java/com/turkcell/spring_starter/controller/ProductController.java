aba6bd40046b8284a9a026e8a2d77c3cce8a3f80
Ayşe Ulaşlı <ayseulasli2727@hotmail.com>
2026-04-14 22:12:12 +0300

    add product

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.turkcell.spring_starter.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.turkcell.spring_starter.model.Product;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -15,13 +21,20 @@ public class ProductController {
     //kullanıcı ne zaman /api/product alanına istek atarsa -> cevap bu fonksiyonda dönen cevap olsun
     //api/product -> sayHi(), matchle
     //HTTP METHOD -> GET, POST, PUT, DELETE,PATCH...
-    @GetMapping("")
-    public String sayHi(){
-        return "Hello";
+    @GetMapping("") //controllerın uzantısı + getin uzantısı -> /api/product
+    public String sayHi(String name, int age){ //localhost:8080/api/product?name=John&age=30
+        return "Hi " + name + ", you are " + age + " years old.";
     }
 
-    @GetMapping("Hello")
-    public String sayHi2(){
-        return "Hello2";
+    @GetMapping("hello/{name}/{age}")//localhost:8080/api/product/hello/John/30
+    public String sayHelloPathVariable(@PathVariable String name, @PathVariable int age){
+        return "Hello " + name + ", you are " + age + " years old.";
+    }
+
+
+    @PostMapping
+    public Product add(@RequestBody Product product){ //JSON -> Java
+        //veritabanına kaydetme işlemi yapılır
+        return product; //kaydedilen ürün geri döner
     }
 }
